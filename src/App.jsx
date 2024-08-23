41ddc1672dcd0ceb76797dc824a5e7dd5e137edc
Johan <105075095+Jagc-1@users.noreply.github.com.>
2024-08-23 16:27:41 -0500

    feat: :fire: Añadiendo y mejorando Cosas

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Project from "./pages/project/Project";
 import About from "./pages/about/About";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 
-
 function App() {
   return (
-    <>
-      <Router>
-        <div className="w-full min-h-screen bg-neutral-950 flex flex-col text-neutral-300">
-          {/* Navbar */}
-          <Navbar />
-         
-    
-          {/* Routes */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/projects" element={<Project />} />
-            <Route path="/me" element={<About />} />
-          </Routes>
+    <Router>
+      <div className="w-full min-h-screen bg-neutral-950 flex flex-col text-neutral-300">
+        {/* Navbar */}
+        <Navbar />
+      
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Project />} />
+          <Route path="/me" element={<About />} />
+        </Routes>
 
-          {/* Footer */}
-          <Footer />
-        </div>
-      </Router>
-    </>
-  )
+        {/* Footer */}
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
