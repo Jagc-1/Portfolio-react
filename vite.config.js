b8bf14d589bc7efd3b8ee63e38ed6d0b706f72f6
Johan <105075095+Jagc-1@users.noreply.github.com.>
2024-08-23 16:34:41 -0500

    Cambios

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Esto asegura que se utilice la raíz del dominio
+  base: '/Portfolio/', // Esto asegura que se utilice la raíz del dominio
 })
